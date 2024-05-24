d13d221ea8c5404f8c54b3426b6a9366b2cab483
Dmitry A. Grechka <dmitry@grechka.family>
2024-05-25 00:50:15 +0400

    Telegram bot interface (#2)
    * telegram chat works
    
    * fixing docker hub push

---
+++ ChatBot/LLMs/Chat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot.LLMs
+{
+    public class Chat
+    {
+        /// <summary>
+        /// e.g. Telegram
+        /// </summary>
+        public string Namespace { get; set; }
+
+        /// <summary>
+        /// Unique identifier for the user within the namespace
+        /// </summary>
+        public string ID { get; set; }
+
+
+        public Chat(string @namespace, string id)
+        {
+            Namespace = @namespace;
+            ID = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Chat chat)
+            {
+                return chat.Namespace == Namespace && chat.ID == ID;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Namespace, ID);
+        }
+
+        public override string ToString()
+        {
+            return $"{Namespace}|{ID}";
+        }
+    }
+}
--- ChatBot/Settings.cs
+++ ChatBot/Settings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatBot.LLMs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,40 @@ namespace ChatBot
 {
     public class Settings
     {
+        public TelegramBotSettings? TelegramBot { get; set; }
+
+        public PersonaConfig? Persona { get; set; }
+
+        public LLMConfig? LLM { get; set; }
+    }
+
+    public class LLMConfig { 
+        public HuggingFaceLLMConfig? HuggingFace { get; set; }
+    }
+
+    public class HuggingFaceLLMConfig
+    {
+        public string ModelName { get; set; }
+        public string ApiKey { get; set; }
+    }
+
+    public class TelegramBotSettings
+    {
+        public string AccessToken { get; set; }
+    }
+
+    public class PersonaConfig
+    {
+        public InlinePersonaConfig? InlineConfig { get; set; }
+
+        // TODO: add database support
+    }
+
+    public class InlinePersonaConfig
+    {
+        public string BotSpecificPrompt { get; set; }
+        public string? UnknownUserPrompt { get; set; }
+
+        public Dictionary<string, string> KnownUsersPrompts { get; set; }
     }
 }
