6b940e112330d2f3cde14bec7f60d686a2eb2d83
Dmitry Grechka <dmitry@grechka.family>
2024-06-08 23:12:40 +0400

    prompt compiler

--- Tests/Prompt.cs
+++ Tests/Prompt.cs
@@ -1,4 +1,6 @@
 ﻿using ChatBot.Prompt;
+using Xunit;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -65,6 +67,29 @@ namespace Prompt
                 { "name", "John" },
                 { "bot_name", "ChatBot" }
             }));
+        }
+
+        [Fact]
+        [Trait(Attributes.TestCategory, Attributes.UnitTestCategory)]
+        public async Task Compiler_Graph1()
+        {
+            var templateSourceMock = new Mock<ITemplateSource>();
+            templateSourceMock.Setup(x => x.HasKey("abc")).ReturnsAsync(true);
+            templateSourceMock.Setup(x => x.HasKey("de")).ReturnsAsync(true);
+            templateSourceMock.Setup(x => x.HasKey("fghi")).ReturnsAsync(true);
+            templateSourceMock.Setup(x => x.HasKey("jkl")).ReturnsAsync(true);
+
+            templateSourceMock.Setup(x => x.GetValue("abc")).ReturnsAsync("Abc references [◄de►] and [◄fghi►].");
+            templateSourceMock.Setup(x => x.GetValue("de")).ReturnsAsync("De references |◄fghi►|.");
+            templateSourceMock.Setup(x => x.GetValue("fghi")).ReturnsAsync("Fghi references (◄jkl►).");
+            templateSourceMock.Setup(x => x.GetValue("jkl")).ReturnsAsync("Jkl references nothing.");
+
+
+            var compiler = new ChatBot.Prompt.Compiler(null, new List<ITemplateSource> {templateSourceMock.Object});
+
+            var result = await compiler.CompilePrompt("abc");
+
+            Assert.Equal("Abc references [De references |Fghi references (Jkl references nothing.).|.] and [Fghi references (Jkl references nothing.).].", result);
         }
     }
 }
