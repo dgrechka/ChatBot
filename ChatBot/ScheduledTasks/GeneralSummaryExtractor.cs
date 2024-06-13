e70483eedf3c3bc7992418d26570775772509d10
Dmitry Grechka <dmitry@grechka.family>
2024-06-14 02:35:33 +0400

    missing files

---
+++ ChatBot/ScheduledTasks/GeneralSummaryExtractor.cs
@@ -0,0 +1,68 @@
+﻿using ChatBot.Interfaces;
+using ChatBot.LLMs;
+using ChatBot.Prompt;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot.ScheduledTasks
+{
+    public class GeneralSummaryExtractorScoped : ConversationProcessorScoped
+    {
+        private readonly IPromptCompiler _promptCompiler;
+        private readonly ILLM _llm;
+
+        public GeneralSummaryExtractorScoped(
+            ILogger<ConversationProcessorScoped> logger,
+            ISummaryStorage summaryStorage,
+            IChatHistoryReader chatHistoryReader,
+            IPromptCompiler promptCompiler,
+            UserMessageContext context,
+            ILLM llm,
+            ConversationProcessingSettings settings)
+            : base(logger, summaryStorage, chatHistoryReader, settings, context, "Summary")
+        {
+            _promptCompiler = promptCompiler;
+            _llm = llm;
+        }
+
+        protected override async Task ProcessCore(IEnumerable<Message> conversation, CancellationToken cancellationToken)
+        {
+
+            var formattedConversation = Helpers.FormatConversation(
+                conversation,
+                out DateTime? firstMessageTime,
+                out DateTime? lastMessageTime,
+                out int counter);
+
+            var runtimeTemplates = new Dictionary<string, string>
+            {
+                { "conversation-to-process", formattedConversation.ToString() }
+            };
+
+            var prompt = await _promptCompiler.CompilePrompt("llama3-conversation-summary", runtimeTemplates,  cancellationToken);
+            
+            var accountingInfo = new AccountingInfo(_context.Chat, "ConversationSummary");
+            var callSettings = new CallSettings()
+            {
+                StopStrings = ["<|eot_id|>", "\n```" ],
+            };
+
+            var summary = await _llm.GenerateResponseAsync(prompt, accountingInfo, callSettings, cancellationToken);
+
+            var summaryHeader = $"The conversation started at {Helpers.FormatTimestamp(firstMessageTime)},\nlasted for {Math.Round((lastMessageTime-firstMessageTime).Value.TotalMinutes)} minutes and consisted of {counter} messages.\n\n";
+
+            var wholeSummary = summaryHeader + summary;
+
+            _logger?.LogInformation($"Generated summary for conversation with {counter} messages. generated length {wholeSummary.Length}");
+
+            _logger?.LogDebug(wholeSummary);
+            // save summary to storage
+            await _summaryStorage.SaveSummary(_context.Chat, _summaryId, lastMessageTime.Value, wholeSummary, cancellationToken);
+        }
+    }
+}
---
+++ ChatBot/ScheduledTasks/Helpers.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatBot.LLMs;
+using Microsoft.VisualBasic;
+
+namespace ChatBot.ScheduledTasks
+{
+    public class Helpers
+    {
+        public static async IAsyncEnumerable<List<Message>> ClusterConversations(IAsyncEnumerable<Message> messages, TimeSpan idleConversationTime)
+        {
+            // then we cluster them based on their timestamp
+            List<Message> conversation = new();
+            DateTime? prevMessageTime = null;
+
+            var now = DateTime.UtcNow;
+
+            await foreach (var message in messages)
+            {
+                if (now - message.Timestamp < idleConversationTime)
+                {
+                    // the conversation is still ongoing. will not process it yet
+                    yield break;
+                }
+
+                if (conversation.Count != 0 && message.Timestamp - prevMessageTime > idleConversationTime)
+                {
+                    // process the conversation
+                    yield return conversation;
+                    conversation = new();
+                }
+
+                conversation.Add(message);
+                prevMessageTime = message.Timestamp;
+            }
+
+            if (conversation.Count != 0)
+            {
+                yield return conversation;
+            }
+        }
+
+        public static string FormatConversation(IEnumerable<Message> conversation, out DateTime? firstMessageTime, out DateTime? lastMessageTime, out int messageCounter) {
+            StringBuilder convSB = new();
+            bool first = true;
+            firstMessageTime = null;
+            lastMessageTime = null;
+            messageCounter = 0;
+            foreach (var message in conversation)
+            {
+                if (first)
+                {
+                    convSB.AppendLine($"[start of conversation at {FormatTimestamp(message.Timestamp)}]");
+                    firstMessageTime = message.Timestamp;
+                    first = false;
+                }
+
+                convSB.AppendLine($"{message.Author}:\t{message.Content}\n");
+                lastMessageTime = message.Timestamp;
+                messageCounter++;
+            }
+
+            convSB.AppendLine($"[end of conversation at {FormatTimestamp(lastMessageTime) ?? string.Empty}]");
+
+            return convSB.ToString();
+        }
+
+
+        public static string FormatTimestamp(DateTime? timestamp)
+        {
+            if (timestamp.HasValue)
+            {
+                return timestamp.Value.ToString("yyyy-MM-dd HH:mm:ssZ (ddd)", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
